e9d679cada4dc5961b5cf802a493bc9af94ad364
Lucas <mariolucas.2000@outlook.com>
2022-10-10 20:52:32 -0300

    :sparkles: feat:

--- src/main/java/com/mlucas/dscommerce/entities/OrderItem.java
+++ src/main/java/com/mlucas/dscommerce/entities/OrderItem.java
@@ -1,7 +1,11 @@
 package com.mlucas.dscommerce.entities;
 
 import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
 
   @EmbeddedId
