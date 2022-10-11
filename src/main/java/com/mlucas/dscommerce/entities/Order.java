16a15c985c58fe6725cdf94bfce1659738f14937
Lucas <mariolucas.2000@outlook.com>
2022-10-10 21:26:36 -0300

    :sparkles: feat: OrderItem and OrderItemPK

--- src/main/java/com/mlucas/dscommerce/entities/Order.java
+++ src/main/java/com/mlucas/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.mlucas.dscommerce.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -79,6 +80,14 @@ public class Order {
 
   public void setClient(User client) {
     this.client = client;
+  }
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Product> getProducts() {
+    return items.stream().map(x -> x.getProduct()).toList();
   }
 
 }
--- src/main/java/com/mlucas/dscommerce/entities/Product.java
+++ src/main/java/com/mlucas/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.mlucas.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -90,6 +91,14 @@ public class Product {
 
   public Set<Category> getCategories() {
     return this.categories;
+  }
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Order> getOrders() {
+    return items.stream().map(x -> x.getOrder()).toList();
   }
 
 }
